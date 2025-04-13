abfd7ba993b9ba984667a07b12b5a52e76fc7ecf
RastaDjuss <folienesie@google.com>
2025-04-13 08:45:16 -0400

    Clean reset of repository

---
+++ Auth0LockPasswordless.js
@@ -0,0 +1,3 @@
+import { Auth0LockPasswordless } from 'auth0-lock';
+
+const lock = new Auth0LockPasswordless('8Bpir6t0QyLRpl5OiRFNOHFAaX64zY7t', 'dev-vot06ciuc1c8bvlb.ca.auth0.com');
---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+/* eslint.config.js */
+import { configs as nextConfigs } from '@next/eslint-plugin-next';
+
+export default [
+  ...nextConfigs.recommended, // Règles Next.js recommandées
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      // Ajoutez vos règles personnalisées ici
+      'react/react-in-jsx-scope': 'off', // React 17+ n'en a pas besoin
+      '@typescript-eslint/no-unused-vars': 'warn', // Avertissement pour les variables inutilisées
+    },
+  },
+];
